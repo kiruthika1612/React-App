91aa07ec9a2bce7336b82495c9bb176e386c2723
kiruthika1612 <56331587+kiruthika1612@users.noreply.github.com>
2025-04-15 17:32:26 -0400

    added image from folder and github profile pic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import chef from "./images/chef.jpg";
 
 let language = "Donavan's Kitchen";
 let moon = "🍛";
@@ -28,29 +29,43 @@ function Header2({name, month, year})
 
   function Receipe1(){
   return(
+    <main>
+      <img src="https://github.com/kiruthika1612.png"
+      height={200} alt="mypic from git hub profile"  /> 
     <ul>
       <li>Idly n Sambhar</li>
       <li>Briyani</li>
       <li>kothu Parota</li>
     </ul>
-
+    </main>
   );
 }
 
 const receipes = [
   "Tasty meatball Pasta",
   "Veg Spagetti",
-  "Quail Fry"
+  "Quail Fry",
+  "Muscles dry gravy"
 ];
+
+const receipeObjects = receipes.map((dish,i)=>({
+  id:i,
+  title:dish
+}));
+
+console.log(receipeObjects);
 
 function Receipe2({dishes})
 {
   return(
+    <main>
+    <img src={chef} height={200} alt="Pic of a Chef"/>
     <ul>
 {dishes.map((dish)=>(
-  <li style={{listStyleType: "none"}}>{dish}</li>
+  <li key={dish.id} style={{listStyleType: "none"}}>{dish.title}</li>
 ))}
     </ul>
+    </main>
   );
 }
 
@@ -62,7 +77,7 @@ function App() {
       <Header name="Kiruthika" year = {1994} month={new Date().getMonth()}/>
       <Receipe1 />
       <Header2 name="Nishanth" year = {1993} month={new Date().getMonth()}/>
-      <Receipe2 dishes={receipes}/>
+      <Receipe2 dishes={receipeObjects}/>
     </div>
     
     
